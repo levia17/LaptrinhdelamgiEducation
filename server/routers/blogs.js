fadac0c7d26c2402060c3dabefc6b525478b4566
QuocHung <hungthanh1412hz@gmail.com>
2024-03-05 13:40:21 +0700

    branch server

--- server/routers/blogs.js
+++ server/routers/blogs.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const BlogsModel = require("../models/blogs");
 
-
 // Admin
 router.get("/", (req, res, next) => {
   BlogsModel.find({})
@@ -25,13 +24,20 @@ router.post("/", (req, res, next) => {
 });
 
 router.delete("/:id", (req, res) => {
-  const blogID = req.params.id;
+  const id = req.params.id;
 
   BlogsModel.deleteOne({
-    blogID: blogID,
+    _id: id,
   })
     .then((data) => res.json("Da xoa bai viet"))
     .catch((err) => res.json("Loi!"));
+});
+
+router.get("/page", (req, res) => {
+  BlogsModel.find()
+    .limit(1)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
 // User
@@ -44,11 +50,5 @@ router.delete("/:id", (req, res) => {
     .then((data) => res.json("Da xoa bai viet"))
     .catch((err) => res.json("Loi!"));
 });
-
-
-
-
-
-
 
 module.exports = router;
